36aef857976e638ca247b309649adf67d4b6f023
Dima <dmitri.maximov@gmail.com>
2019-10-11 16:16:22 +0500

    Adds support for review request added/removed events

--- Octokit/Models/Response/ActivityPayloads/PullRequestEventPayload.cs
+++ Octokit/Models/Response/ActivityPayloads/PullRequestEventPayload.cs
@@ -8,6 +8,7 @@ namespace Octokit
         public string Action { get; protected set; }
         public int Number { get; protected set; }
         public Label Label { get; protected set; }
+        public User RequestedReviewer { get; protected set; }
 
         public PullRequest PullRequest { get; protected set; }
     }
